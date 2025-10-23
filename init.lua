de4e18c09b4c89786e83e72b9d566983945b677e
Eugene Briones <eugenebriones7135@gmail.com>
2025-10-23 21:04:39 +1100

    autocomplete to tab (im so sorry)

--- init.lua
+++ init.lua
@@ -864,7 +864,8 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'default',
+        -- preset = 'default',
+        preset = 'super-tab', -- im sorry but <C-y> is hard to reach
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
